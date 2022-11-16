3744ff3dfd6f6c3c156f2e345a80631b45aea45c
Elif Özkan <elffozkann@gmail.com>
2022-11-16 10:59:13 +0300

    Manav Kasa Programı

---
+++ src/ManavKasaProgrami.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ManavKasaProgrami {
+    public static void main(String[] args) {
+        double A = 2.14, E = 3.67, D = 1.11, M = 0.95, P = 5.00;
+        double Armut, Elma, Domates, Muz, Patlican;
+        double total;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Armut Kaç Kilo ?: ");
+        Armut = inp.nextDouble();
+        System.out.print("Elma Kaç Kilo ?: ");
+        Elma = inp.nextInt();
+        System.out.print("Domates Kaç Kilo ?: ");
+        Domates = inp.nextInt();
+        System.out.print("Muz Kaç Kilo ?: ");
+        Muz = inp.nextInt();
+        System.out.print("Patlıcan Kaç Kilo ?: ");
+        Patlican = inp.nextInt();
+
+        total = (A*Armut) + (E*Elma) + (D*Domates) + (M*Muz) + (P*Patlican);
+        System.out.println("Toplam Tutar: " + total);
+    }
+}
--- src/VucutKitleIndex.java
+++ src/VucutKitleIndex.java
@@ -2,23 +2,17 @@ import java.util.Scanner;
 
 public class VucutKitleIndex {
     public static void main(String[] args) {
-        double A = 2.14, E = 3.67, D = 1.11, M = 0.95, P = 5.00;
-        double Armut, Elma, Domates, Muz, Patlican;
-        double total;
+        int kg;
+        double m;
+        double index;
 
         Scanner inp = new Scanner(System.in);
-        System.out.print("Armut Kaç Kilo ?: ");
-        Armut = inp.nextDouble();
-        System.out.print("Elma Kaç Kilo ?: ");
-        Elma = inp.nextInt();
-        System.out.print("Domates Kaç Kilo ?: ");
-        Domates = inp.nextInt();
-        System.out.print("Muz Kaç Kilo ?: ");
-        Muz = inp.nextInt();
-        System.out.print("Patlıcan Kaç Kilo ?: ");
-        Patlican = inp.nextInt();
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+        m = inp.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kg = inp.nextInt();
 
-        total = (A*Armut) + (E*Elma) + (D*Domates) + (M*Muz) + (P*Patlican);
-        System.out.println("Toplam Tutar: " + total);
+        index = kg / ( m * m );
+        System.out.println("Vücut Kitle İndeksiniz: " + index);
     }
 }
