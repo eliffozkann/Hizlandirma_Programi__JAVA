5ab4e784442cc95544e1bc0d04317d0bca34042e
Elif Özkan <elffozkann@gmail.com>
2022-11-17 00:38:17 +0300

    changes

--- src/ArtikYil.java
+++ src/ArtikYil.java
@@ -4,6 +4,13 @@ public class ArtikYil {
     public static void main(String[] args) {
         int n;
 
+        for (int k = 0; k < 9; k++) {
+            System.out.print("*");
+            if (k == 3)
+                break;
+        }
+
+
         Scanner inp = new Scanner(System.in);
         System.out.print("Yıl Giriniz: ");
         n = inp.nextInt();
