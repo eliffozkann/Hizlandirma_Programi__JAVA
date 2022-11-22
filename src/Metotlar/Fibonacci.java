18ac65f0adabd1a702b7db7e0815982aef8a146c
Elif Özkan <elffozkann@gmail.com>
2022-11-23 02:05:59 +0300

    fibonacci

---
+++ src/Metotlar/Fibonacci.java
@@ -0,0 +1,14 @@
+package Metotlar;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(5));
+    }
+    static int fibonacci(int n){
+        if(n == 1 || n == 2){
+            return 1;
+        }else{
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+}
