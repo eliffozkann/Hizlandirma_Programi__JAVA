6cbf13f136e6dc73edddb81d273da148de5f11ea
Elif Özkan <elffozkann@gmail.com>
2022-11-29 23:17:41 +0300

    yıldızdan ğçgen yapma

--- src/DiamondWithStars.java
+++ src/DiamondWithStars.java
@@ -28,3 +28,27 @@ public class DiamondWithStars {
 
     }
 }
+/*
+
+import java.util.Scanner;
+
+public class DiamondWithStars {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Üçgen Satır Sayısı: ");
+        int sayi = inp.nextInt();
+
+        for (int i = 0; i <= sayi ; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j < sayi; j++) {
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+*/
+
+
