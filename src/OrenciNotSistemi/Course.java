9ba8b9b38534942ce8aa8c26f987d609a81840ac
Elif Özkan <elffozkann@gmail.com>
2022-12-08 00:55:30 +0300

    ogrenci not sistemi yapılması

---
+++ src/OrenciNotSistemi/Course.java
@@ -0,0 +1,32 @@
+package OrenciNotSistemi;
+
+public class Course {
+    Teacher teacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+
+    Course(String name, String code, String prefix){
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+    }
+
+    void addTeacher(Teacher teacher){
+        if (teacher.branch.equals(this.prefix)){
+            this.teacher = teacher;
+            printTeacher();
+        }else {
+            System.out.println("Öğretmen ve ders bilgileri uyuşmuyor.");
+        }
+        this.teacher =teacher;
+        printTeacher();
+    }
+
+    void  printTeacher(){
+        this.teacher.print();
+    }
+
+}
